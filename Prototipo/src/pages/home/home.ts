c2be44529b7458590075d37dffee60d97b1b9779
Unknown <100363812@alumnos.uc3m.es>
2019-04-05 16:58:47 +0200

    prototipo - firebase
    Conexion con firebase correcta

--- Prototipo/src/pages/home/home.ts
+++ Prototipo/src/pages/home/home.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {FirebaseDbProvider} from '../../providers/firebase-db/firebase-db';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+	listaClientes:any;
 
-  constructor(public navCtrl: NavController) {
 
+  constructor(public navCtrl: NavController,public dbFirebase:FirebaseDbProvider) {
+	
+  }
+  
+   hola(){
+	  this.dbFirebase.getClientes();
   }
 
 }
--- Prototipo/src/providers/firebase-db/firebase-db.ts
+++ Prototipo/src/providers/firebase-db/firebase-db.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+//import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { AngularFireDatabase} from 'angularfire2/database';
 
 /*
   Generated class for the FirebaseDbProvider provider.
@@ -10,8 +11,21 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class FirebaseDbProvider {
 
-  constructor(public http: HttpClient) {
+  /*constructor(public http: HttpClient) {
+    console.log('PROVIDER MALO');
+  }*/
+  
+	constructor(public afDB:AngularFireDatabase) {
     console.log('Hello FirebaseDbProvider Provider');
   }
+  
+  private clientesRef=this.afDB.list<Cliente>('clientes');
+	 getClientes()
+	 {
+		//return this.clientesRef.valueChanges();
+		 this.afDB.database.ref('javi').set('ta tonto');
+		console.log("llamando");
+	 } 
+  
 
 }
